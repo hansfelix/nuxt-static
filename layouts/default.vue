a821b81c79f9ce79373f696bd07e163e9a4bbd83
Hans Felix <hansfelix50@gmail.com>
2019-10-12 01:11:27 -0500

    mejora de gracias page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class>
-    <NavBar/>
-    <Nuxt/>
-    <Footer/>
+  <div class="default-layout">
+    <NavBar />
+    <Nuxt class="default-layout__content" />
+    <Footer />
   </div>
 </template>
 
@@ -18,23 +18,35 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+/*------------------------------------*\
+  .default-layout
+\*------------------------------------*/
+.default-layout {
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr auto;
+
+  &__content {
+    padding-top: 76px;
+  }
+}
+
 :root {
   --main-color-1: #c3cf6a;
   --main-color-2: #91b053;
   --main-color-3: #6c8e50;
 }
 
-body{
+body {
   margin: 0;
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
 }
 
-body p { 
+body p {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
     "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
 }
-
 </style>
 
--- pages/gracias.vue
+++ pages/gracias.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <h1>Gracias</h1>
-    <p>Nos pondremos en contacto con UD.</p>
+  <div class="gracias-page container">
+    <div class="gracias-page_mensaje">
+      <h1>Gracias</h1>
+      <p>Hemos recibido tu interés en el simposio</p>
+      <p>Nos pondremos en contacto con usted.</p>
+    </div>
   </div>
 </template>
 
@@ -10,3 +13,13 @@ export default {
   layout: "default"
 };
 </script>
+
+<style lang="scss">
+/*------------------------------------*\
+  .gracias-page
+\*------------------------------------*/
+.gracias-page {
+  display: flex;
+  align-items: center;
+}
+</style>
